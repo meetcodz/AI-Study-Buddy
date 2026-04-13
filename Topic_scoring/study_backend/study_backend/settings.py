a8e8f8a060226166820e74a935c5c41a1e56c19b
Meet Modi <modimeet2007@gmail.com>
2026-04-14 00:01:31 +0530

    Fix CSRF_TRUSTED_ORIGINS empty string error during collectstatic

--- Topic_scoring/study_backend/study_backend/settings.py
+++ Topic_scoring/study_backend/study_backend/settings.py
@@ -57,7 +57,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:8000',
     'http://127.0.0.1:8000',
-] + os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+] + [url.strip() for url in os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',') if url.strip()]
 
 
 # Application definition
